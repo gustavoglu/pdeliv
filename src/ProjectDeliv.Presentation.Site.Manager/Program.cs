2c184786d463d17bcc90fd3aa4fb3a5474a2a63d
Gustavo <gustavoglu@hotmail.com>
2021-12-03 09:50:37 -0300

    resolvendo bugs e alteracoes no blazor

--- src/ProjectDeliv.Presentation.Site.Manager/Program.cs
+++ src/ProjectDeliv.Presentation.Site.Manager/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ProjectDeliv.Presentation.Site.Manager;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.Services.AddMudServices();
+
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
